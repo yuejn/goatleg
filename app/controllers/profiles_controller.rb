4e7321a1923608707c1a214ac4023b749fa97ff5
Fabian Rinaldi <code.optica@gmail.com>
2015-02-11 16:25:49 +0800

    first stage profile form,its already able to save data

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,30 @@
 class ProfilesController < ApplicationController
+  # before_action :authenticate_user!
+  
+  def register
+    @profile = Profile.new
+    @profile.user_id = 1 
+    # @profile.user_id = current_user.id
+  end
+
+  def create
+    @profile = Profile.new(profile_params) 
+    
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to "http://www.google.com"}
+      else
+        format.html { render :register}
+      end
+    end
+  end
+
+  def show
+  end
+
+private
+  def profile_params
+      params.require(:profile).permit(:user_id,:gender, :seeking_gender, :age)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'profiles/register' => 'profiles#register'
+  post 'profiles/create' => 'profiles#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
