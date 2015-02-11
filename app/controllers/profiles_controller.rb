ef630b3c9d8c0a0a419c044b95e53337f0022f8d
yuejn <jennifer8ho@gmail.com>
2015-02-11 14:37:38 +0800

    basic index/show methods to trips and profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,11 @@
 class ProfilesController < ApplicationController
+
+  def index
+    @profiles = Profile.all
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,11 @@
 class TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
 end
