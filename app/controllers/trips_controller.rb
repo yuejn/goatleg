d7e14bf9b1bb81fad805bb4ec3a87a6afe6c850d
yuejn <jennifer8ho@gmail.com>
2015-02-11 15:53:58 +0800

    trips and stuff

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,4 +8,32 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    respond_to do |format|
+      if @trip.save
+        format.html { redirect_to @trip, notice: 'Trip was successfully addded to yo itinerary!' }
+        format.json { render :show, status: :created, location: @trip }
+      else
+        format.html { render :new }
+        format.json { render json: @trip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:user_id, :destination, :start_date, :end_date)
+  end
+
 end
