baa1c28ee2623dcdd3e1930bf1d5f1876b58bbf5
Dan Otto <theDanOtto@gmail.com>
2015-02-12 13:44:57 +0800

    create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  #validates :eaten_goatleg, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
