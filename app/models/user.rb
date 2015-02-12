4c76ebe57504e5bcb0c7e7e74385fe4ee6ef7eac
Dan Otto <theDanOtto@gmail.com>
2015-02-12 16:35:45 +0800

    create profile on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #validates :eaten_goatleg, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile
   has_many :trips
   has_many :messages
+  accepts_nested_attributes_for :profile
+
+  after_create :add_profile
+
+
+  def add_profile
+    Profile.create(user_id:self.id)
+    #byebug
+  end
+
+
 
 end
